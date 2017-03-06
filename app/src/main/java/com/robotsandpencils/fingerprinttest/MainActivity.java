c8b752c412b0363a3dee7607b92611bf6724d33c
Neal Sanche <neal.sanche@robotsandpencils.com>
2017-03-06 16:41:36 -0700

    Updated for Android Studio 2.3

--- app/src/main/java/com/robotsandpencils/fingerprinttest/MainActivity.java
+++ app/src/main/java/com/robotsandpencils/fingerprinttest/MainActivity.java
@@ -10,10 +10,13 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.mattprecious.swirl.SwirlView;
 import com.mtramin.rxfingerprint.RxFingerprint;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int DELAY_MILLIS = 1500;
     private FloatingActionButton mFab;
 
     @Override
@@ -27,38 +30,44 @@ public class MainActivity extends AppCompatActivity {
         mFab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
 
-        /*
-        Subscription subscription = RxFingerprint.authenticate(this)
-                .subscribe(fingerprintAuthenticationResult -> {
-                    switch (fingerprintAuthenticationResult.getResult()) {
-                        case FAILED:
-                            setStatusText("Fingerprint not recognized, try again!");
-                            break;
-                        case HELP:
-                            setStatusText(fingerprintAuthenticationResult.getMessage());
-                            break;
-                        case AUTHENTICATED:
-                            setStatusText("Successfully authenticated!");
-                            break;
-                    }
-                }, throwable -> {
-                    Log.e("ERROR", "authenticate", throwable);
-                });
-                */
+        MaterialDialog dialog = new MaterialDialog.Builder(this)
+                .title(R.string.title)
+                .customView(R.layout.fingerprint_view, false)
+                .positiveText(R.string.use_password)
+                .negativeText(R.string.cancel)
+                .show();
+
+        SwirlView swirlView = (SwirlView) dialog.findViewById(R.id.swirl_view);
 
         SharedPreferences prefs = getSharedPreferences("auth", MODE_PRIVATE);
+
+        swirlView.setState(SwirlView.State.ON, true);
 
         if (!prefs.contains("password")) {
             RxFingerprint.encrypt(this, "password")
                     .subscribe(fingerprintEncryptionResult -> {
                         switch (fingerprintEncryptionResult.getResult()) {
                             case FAILED:
+                                swirlView.setState(SwirlView.State.ERROR, true);
+                                swirlView.postDelayed(() -> {
+                                    swirlView.setState(SwirlView.State.ON, true);
+                                }, DELAY_MILLIS);
                                 setStatusText("Fingerprint not recognized, try again!");
                                 break;
                             case HELP:
+                                swirlView.setState(SwirlView.State.ERROR, true);
+                                swirlView.postDelayed(() -> {
+                                    swirlView.setState(SwirlView.State.ON, true);
+                                }, DELAY_MILLIS);
                                 setStatusText(fingerprintEncryptionResult.getMessage());
                                 break;
                             case AUTHENTICATED:
+                                swirlView.post(() -> {
+                                    swirlView.setState(SwirlView.State.OFF, true);
+                                });
+                                swirlView.postDelayed(() -> {
+                                    dialog.dismiss();
+                                }, DELAY_MILLIS);
                                 setStatusText("Successfully authenticated!");
                                 Log.e("ENCRYPTED", fingerprintEncryptionResult.getEncrypted());
 
@@ -73,12 +82,28 @@ public class MainActivity extends AppCompatActivity {
                     .subscribe(fingerprintDecryptionResult -> {
                                 switch (fingerprintDecryptionResult.getResult()) {
                                     case FAILED:
+                                        swirlView.setState(SwirlView.State.ERROR, true);
+                                        swirlView.postDelayed(() -> {
+                                            swirlView.setState(SwirlView.State.ON, true);
+                                        }, DELAY_MILLIS);
                                         setStatusText("Fingerprint not recognized, try again!");
                                         break;
                                     case HELP:
+                                        swirlView.post(() -> {
+                                            swirlView.setState(SwirlView.State.ERROR, true);
+                                            swirlView.postDelayed(() -> {
+                                                swirlView.setState(SwirlView.State.ON, true);
+                                            }, DELAY_MILLIS);
+                                        });
                                         setStatusText(fingerprintDecryptionResult.getMessage());
                                         break;
                                     case AUTHENTICATED:
+                                        swirlView.post(() -> {
+                                            swirlView.setState(SwirlView.State.OFF, true);
+                                        });
+                                        swirlView.postDelayed(() -> {
+                                            dialog.dismiss();
+                                        }, DELAY_MILLIS);
                                         setStatusText("Successfully authenticated!");
                                         Log.e("DECRYPTED", fingerprintDecryptionResult.getDecrypted());
                                         break;
